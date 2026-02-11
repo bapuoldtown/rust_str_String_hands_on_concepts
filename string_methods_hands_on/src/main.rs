9a7fff9f09f544bfd3c7b7ab8d2463eb8c75ed63
bapuoldtown <bapucodetest1987@gmail.com>
2026-02-11 07:02:51 +0530

    more and more methods

--- string_methods_hands_on/src/main.rs
+++ string_methods_hands_on/src/main.rs
@@ -3,4 +3,14 @@ fn main() {
     println!("Hello, world!");
     println!("The string is with to_str method: {}", string_methods_demo::convert_to_owned("Hello, Rust!"));
     println!("The string is with to owned: {}", string_methods_demo::convert_to_owned_using_method("Hello, Rust!"));
+    println!("The length of the string is: {}", string_methods_demo::string_len("Hello, Rust!"));
+    println!("Is the string empty? {}", string_methods_demo::string_is_empty("Hello, Rust!"));
+    println!("Does the string contain 'Rust'? {}", string_methods_demo::string_contains("Hello, Rust!", "Rust"));
+    println!("Trimmed string: '{}'", string_methods_demo::string_trim("   Hello, Rust!   "));
+    println!("Characters in the string: {:?}", string_methods_demo::string_bytes_collect("Hello, Rust!"));
+    println!("Lines in the string: {:?}", string_methods_demo::string_lines_collect("Hello, 
+    Rust!
+    Hello 
+    again"));
+    println!("Split string by pattern: {:?}", string_methods_demo::split_string_pattern("Hello, Rust! Hello again!", " "));
 }
--- string_methods_hands_on/src/string_methods_demo.rs
+++ string_methods_hands_on/src/string_methods_demo.rs
@@ -1,3 +1,11 @@
+/*
+**Why?** Because `String` automatically **derefs** to `&str`!
+```
+String  ─── Deref ───►  &str
+        (automatic!)
+        All mehhods on &tr and String are in sync bruv
+ */
+
 pub fn convert_to_owned(s: &str) -> String{
     return s.to_string();
 
@@ -5,3 +13,37 @@ pub fn convert_to_owned(s: &str) -> String{
 pub fn convert_to_owned_using_method(s: &str) -> String{
     return s.to_owned();
 }
+
+pub fn string_len(s: &str) -> usize{
+    return s.len();
+}
+
+pub fn string_is_empty(s: &str) -> bool{
+    return s.is_empty();
+}
+
+
+pub fn string_contains(s: &str, substring: &str) -> bool{
+    return s.contains(substring)
+}
+
+pub fn string_trim(s: &str) -> String{
+    return s.trim().to_string();
+}
+
+pub fn string_chars_collect(s: &str) -> Vec<char>{
+    return s.chars().collect::<Vec<char>>();
+}
+
+pub fn string_bytes_collect(s: &str) -> Vec<u8>{
+    return s.bytes().collect::<Vec<u8>>();
+}
+
+pub fn string_lines_collect(s: &str) -> Vec<&str>{
+    return s.lines().collect::<Vec<&str>>();
+}
+
+pub fn split_string_pattern<'a>(s: &'a str, pattern: &str) -> Vec<&'a str>{
+    return s.split(pattern).collect::<Vec<&'a str>>();
+}
+
