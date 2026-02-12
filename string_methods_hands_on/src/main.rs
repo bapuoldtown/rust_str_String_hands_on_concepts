38a1a6d7656e070bf3b10b91b6fbf035058a86a4
bapuoldtown <bapucodetest1987@gmail.com>
2026-02-12 06:59:54 +0530

    adding more string methods

--- string_methods_hands_on/src/main.rs
+++ string_methods_hands_on/src/main.rs
@@ -13,4 +13,30 @@ fn main() {
     Hello 
     again"));
     println!("Split string by pattern: {:?}", string_methods_demo::split_string_pattern("Hello, Rust! Hello again!", " "));
+    println!("Find substring 'Rust' in main string: {:?}", string_methods_demo::find_substring("Hello, Rust! Hello again!", "Rust"));  
+    match string_methods_demo::find_substring("Hello Rust World","Rust"){
+        Some(index) => println!("Substring found at index: {}", index),
+        None => println!("Substring not found"),
+    }
+    //check if for match with Some else None
+    if let Some(index) = string_methods_demo::rfind_demo("Hello World Guru", "Guru"){
+        println!("Substring found at index: {}", index);
+    } else {
+        println!("Substring not found");    
+    }
+    println!("Does the string start with 'Hello'? {}", string_methods_demo::starts_with_demo("Hello, Rust!", "Hello")); 
+    println!("Does the string end with 'Rust!'? {}", string_methods_demo::ends_with_demo("Hello, Rust!", "Rust!"));
+    println!("Trimmed start: '{}'", string_methods_demo::trim_start_demo("   Hello, Rust!   "));
+    println!("Trimmed end: '{}'", string_methods_demo::trim_end_demo("   Hello, Rust!   "));
+    println!("Trimmed matches: '{}'", string_methods_demo::trim_matches_demo("   Hello, Rust!   ", &' '));
+
+    if let Some((a,b)) = string_methods_demo::to_uppercase_lowercase_demo("Guru junu krisha"){
+        println!("Uppercase: {}, Lowercase: {}", a, b);
+    }
+    else{
+        println!("Error converting to uppercase or lowercase");
+    }
+
+    println!("Replaced string: {}", string_methods_demo::replace_demo("Hello, Rust!", "Rust", "World"));
+    println!("The replace demo count method is {}", string_methods_demo::replace_demo_count("Hello, Rust! Hello again!", "Hello", "Hi", 1));
 }
--- string_methods_hands_on/src/string_methods_demo.rs
+++ string_methods_hands_on/src/string_methods_demo.rs
@@ -47,3 +47,63 @@ pub fn split_string_pattern<'a>(s: &'a str, pattern: &str) -> Vec<&'a str>{
     return s.split(pattern).collect::<Vec<&'a str>>();
 }
 
+pub fn find_substring(main: &str, s:&str)->Option<usize>{
+    return main.find(s);
+
+}
+//rfind() - same but searches from RIGHT
+//fn rfind<P>(&self, pat: P) -> Option<usize>
+pub fn rfind_demo(main: &str, pattern: &str) -> Option<usize>{
+    return main.rfind(pattern);
+}
+
+pub fn starts_with_demo(main: &str, pattern: &str) -> bool{
+    return main.starts_with(pattern);
+}
+
+pub fn ends_with_demo(main: &str, pattern: &str) ->bool{
+    return main.ends_with(pattern)
+}
+//fn trim(&self) -> &str
+//      ▲         ▲
+//      │         └── Returns: borrowed slice (NO allocation!)
+//      └── Takes: &self
+pub fn trim_start_demo(main: &str) -> &str{
+    return main.trim_start();
+}
+
+pub fn trim_end_demo(main: &str) -> &str{
+    return main.trim_end();
+}
+
+pub fn trim_matches_demo<'a>(main: &'a str, pattern: &char) ->&'a str{
+    return main.trim_matches(*pattern)
+}
+
+//Some transform string functions
+//to_uppercase()    │ &self          │ String
+//to_lowercase()    │ &self          │ String
+//replace(from,to)  │ &self,P,&str   │ String
+//replacen(f,t,n)   │ &self,P,&str,n │ String
+//repeat(n)         │ &self, usize   │ String
+pub fn to_uppercase_lowercase_demo(main: &str) -> Option<(String, String)>{
+    return Some((main.to_uppercase(), main.to_lowercase()));
+}
+
+//fn replace<P>(&self, from: P, to: &str) -> String
+//        ▲    ▲        ▲        ▲          ▲
+//        │    │        │        │          └── Returns: NEW String
+//        │    │        │        └── Replacement string
+//        │    │        └── Pattern to find
+//        │    └── Takes: &self
+//        └── Generic over pattern
+
+pub fn replace_demo(main: &str, repl_patter: &str, what_pattern: &str)-> String{
+    return  main.replace(repl_patter, what_pattern);
+
+}
+//fn replacen<P>(&self, from: P, to: &str, count: usize) -> String
+pub fn replace_demo_count(main: &str, repl_pattern: &str, what_pattern: &str, count: usize) -> String{
+    return main.replacen(repl_pattern, what_pattern, count);
+
+}
